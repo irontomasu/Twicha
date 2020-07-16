390d8fe23a7fd14f0e8978877df039dc57829706
nori5 <nagatomo55@outlook.com>
2020-07-16 13:39:01 +0900

    rechange thisrepogitory

--- twicha.py
+++ twicha.py
@@ -2,14 +2,14 @@
 #-*- coding: utf-8 -*-
 import tweepy
 
-#認証を行う
+
 consumer_key = '5oM4yP4oWyzLVKOWZzpC46z7f'
 consumer_secret = 'EwRKQqlBnUwiGZ38u6UMONHli0sNcRnAdKRbogzgXSPvutRhhh'
-access_token = '1199166647455477760-BVDYxHwTOyfVoTZP5sq5AzEcbBrrQk'
-access_secret = 'QJmuywTtvv28ZAFfv2wHikc42bjd6I7iYQUUWOZEeSsAf'
+access_token = '1199166647455477760-4sgRmgoQXbSDWFnnQZuPYDB0cIvTlH'
+access_secret = 'nma2MYHPsYu02BDYEgSFDyH7FMwjR1WuR3DQKA2ItYIn8'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
-#Hello, world!と投稿する
-api.update_status('Hello, world!')
+
+api.update_status('peropero')
